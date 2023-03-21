c82743c1f42a65fbfb0ab99b5c94e69838e25e0c
Kishan Thakkar <thakkarkishan097@gmail.com>
2023-03-20 20:24:53 -0700

    Add types for jwt and bcrypt

--- src/utils/authUtility.ts
+++ src/utils/authUtility.ts
@@ -5,19 +5,19 @@ import { throwError } from "./throwError";
 const SALT_ROUNDS = 10;
 const JWT_SECRET = process.env.JWT_SECRET || "secret";
 
-export const hashPassword = (password) => {
+export const hashPassword = (password: string): Promise<string> => {
   return bcrypt.hash(password, SALT_ROUNDS)
 }
 
-export const comparePassword = (plainPassword, passwordHash) => {
+export const comparePassword = (plainPassword: string, passwordHash: string): Promise<boolean> => {
   return bcrypt.compare(plainPassword, passwordHash);
 }
 
-export const generateToken = (payload) => {
+export const generateToken = (payload: object): string => {
   return jwt.sign(payload, JWT_SECRET);
 }
 
-export const verifyToken = (token) => {
+export const verifyToken = (token: string) => {
   try {
     return jwt.verify(token, JWT_SECRET);
   } catch (error) {
--- src/utils/validateFields.ts
+++ src/utils/validateFields.ts
@@ -4,7 +4,7 @@ export const fieldValitationTypes = {
   email: /^[A-z0-9._%+-]+@[A-z0-9.-]+\.[A-z]{2,}$/,
   password:
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-  date: /[0-9]{2}\/[0-9]{2}\/[0-9]{4}/
+  date: /[0-9]{2}\/[0-9]{2}\/[0-9]{4}/,
 };
 
 export const validateFieldValue = (
@@ -17,21 +17,26 @@ export const validateFieldValue = (
     case "email":
       isMatched = value.match(new RegExp(fieldValitationTypes[type]));
       if (!isMatched)
-        throwError(`Please enter ${fieldName ||"email address"} in valid format.`, 400);
+        throwError(
+          `Please enter ${fieldName || "email address"} in valid format.`,
+          400
+        );
     case "password":
       isMatched = value.match(new RegExp(fieldValitationTypes[type]));
       if (!isMatched)
         throwError(
-          `${fieldName || "Password"} should be of minimum 8 character and should contain atleast one uppercase, lowercase, number and special character.`,
+          `${
+            fieldName || "Password"
+          } should be of minimum 8 character and should contain atleast one uppercase, lowercase, number and special character.`,
           400
         );
-        case "date":
-          isMatched = value.match(new RegExp(fieldValitationTypes[type]));
-          if (!isMatched)
-            throwError(
-              `Please enter ${fieldName || "Date"} in MM/dd/yyyy format.`,
-              400
-            );
+    case "date":
+      isMatched = value.match(new RegExp(fieldValitationTypes[type]));
+      if (!isMatched)
+        throwError(
+          `Please enter ${fieldName || "Date"} in MM/dd/yyyy format.`,
+          400
+        );
     default:
       break;
   }
